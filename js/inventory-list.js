fc410d4b7cee6bf06c179ee2af3af47cf709e7a5
Dan Watson <danwatson00@yahoo.com>
2018-02-01 09:29:24 -0600

    updated inventory-list with comments

--- js/inventory-list.js
+++ js/inventory-list.js
@@ -1,40 +1,4 @@
-/** let inventory = [
-    {
-        id: "000-000000-001",
-        name: "Canon EF 50mm f/1.2 USM Lens",
-        category: "Photo/Video",
-        subcategory: "Lens",
-        specs: "Focal Length: 50mm(80mm Equivalent for Crop Sensors), Apterture Range: f / 1.2 - f / 16, Canon EF Mount, Format Coverage: Full Frame, 1.5 ft. Minimum Focusing Distance, 46º Angle of View, 0.15x Magnification, AF / MF, Filter Size: 72mm, Length: 2.6 in., Weight: 1.2 lbs",
-        manualURL: "http://cdn.static-bl.com/images/manual/50mm-f1-2-L.pdf",
-        description: "The Canon EF 50mm f/1.2L USM Lens boasts very high quality with a very fast maximum aperture that is perfect for low depth-of-field portraiture and still life work. It features a Super Spectra coating to suppress flare and ghosting and is also weather-sealed for safe outdoor shooting. It can be paired with both full frame sensor cameras, such as the Canon 5D Mark III, and with crop frame sensor cameras, such as the Canon 7D Mark II. The focal length equivalent on crop frame sensors is 80mm. This lens is considered among many Canon shooters as a definitive portrait lens, along with the Canon EF 85mm f/1.2L USM Lens.",
-        priceDay: 50,
-        priceWeek: 250,
-        condition: 9,
-        included: "Front Cap, Rear Cap, 72mm filter, Lens Hood",
-        notes: "This lens is compatible with Canon EF mount cameras in both full frame and crop frame sensors. The focal length equivalent on crop frame sensors is 80mm. This lens is not compatible with 2X and 1.4X Canon extenders.",
-        productReview: "",
-        userReviews: ""
-
-    },
-    {
-        id: "000-000000-002",
-        name: "Canon 5D Mark III DSLR Camera Body",
-        category: "Photo/Video",
-        subcategory: "DSLR Camera Body"
-        description: "The Canon EF 50mm f/1.2L USM Lens boasts very high quality with a very fast maximum aperture that is perfect for low depth-of-field portraiture and still life work. It features a Super Spectra coating to suppress flare and ghosting and is also weather-sealed for safe outdoor shooting. It can be paired with both full frame sensor cameras, such as the Canon 5D Mark III, and with crop frame sensor cameras, such as the Canon 7D Mark II. The focal length equivalent on crop frame sensors is 80mm. This lens is considered among many Canon shooters as a definitive portrait lens, along with the Canon EF 85mm f/1.2L USM Lens.",
-        specs: "Focal Length: 50mm(80mm Equivalent for Crop Sensors), Apterture Range: f / 1.2 - f / 16, Canon EF Mount, Format Coverage: Full Frame, 1.5 ft. Minimum Focusing Distance, 46º Angle of View, 0.15x Magnification, AF / MF, Filter Size: 72mm, Length: 2.6 in., Weight: 1.2 lbs",
-        manualURL: "http://cdn.static-bl.com/images/manual/50mm-f1-2-L.pdf",
-        productReview: "",
-        priceDay: 50,
-        priceWeek: 250,
-        condition: 9,
-        included: "camera strap, body cap, battery, charger",
-        notes: "This lens is compatible with Canon EF mount cameras in both full frame and crop frame sensors. The focal length equivalent on crop frame sensors is 80mm. This lens is not compatible with 2X and 1.4X Canon extenders.",
-        productReview: "",
-        userReviews: ""
-    }
-];
-**/
+//This function names the object and the properties of the object
 function newItem(itemId, itemName, itemCategory, itemSubcategory, itemDescription, itemSpecs, itemManualURL, itemProductReview, itemPriceDay, itemPriceWeek, itemCondition, itemIncluded, itemNotes, itemUserReviews){
     this.itemId = itemId;
     this.itemName = itemName;
@@ -57,11 +21,18 @@ function newItem(itemId, itemName, itemCategory, itemSubcategory, itemDescriptio
 
 console.log(newItem);
 
+//Declares the array myGear
 var myGear = [];
+
+//Declares that the element object myGearDiv represents id="myGearDiv"
 myGearDiv = document.getElementById("myGearDiv");
 
+//Declares the varialbe formSubmitBtn to be represented by id="formSubmitBtn"
 var formSubmitBtn = document.getElementById("formSubmitBtn");
+
 console.log(formSubmitBtn);
+
+//Create an event listener that acts on any click to formSubmitBtn
 formSubmitBtn.addEventListener('click', () => {
     let gearItem = {};
         gearItem.itemName = document.getElementById("itemName").value;
@@ -80,15 +51,18 @@ formSubmitBtn.addEventListener('click', () => {
         console.log(myGear);
         
         for (var i = 0; i <= myGear.length; i++) {
-            let gearItemElement = `<div class="gearCard"><img class="itemImage" src"${gearItem.itemImageURL}"<br>
-            <h3>${gearItem.itemName}</h3><p>${gearItem.itemCategory}</p><br><small>${gearItem.itemSubcategory}</small><br>
-            <ul class="cardList"><li>$${gearItem.itemPriceDay}/day</li><li>$${gearItem.itemPriceWeek}/week</li>
-            <li>${gearItem.itemCondition}</li><li>${gearItem.itemId}</li></ul><p>More info</p></div>`;
+            let gearItemElement = `<div class="gearCard"><img class="itemImage" src="${gearItem.itemImageURL}><br><h3>${gearItem.itemName}</h3><p>${gearItem.itemCategory}</p><small>${gearItem.itemSubcategory}</small><br>
+            <ul class="cardList"><li>Price Per Day: $${gearItem.itemPriceDay}</li><li>Price Per Week: $${gearItem.itemPriceWeek}</li>
+            <li>Item Condition: ${gearItem.itemCondition}</li><li>Item ID:${gearItem.itemId}</li></ul><p>More info</p></div>`;
             myGearDiv.innerHTML += gearItemElement;
 }
+/**
+            var gearItemElement = myGear.
+ */
     }
     
 );
+
 
 
 
